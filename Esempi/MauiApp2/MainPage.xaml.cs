b577e4d76ed50814cbb0b185c1618e4c6cbe6799
BohdanPohorielov <vun297@student.edu.ti.ch>
2026-02-04 15:44:45 +0100

    CalcoloDelta

---
+++ Esempi/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        double a, b, c;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(CefA.Text, out a))
+            {
+                Result.Text = "Input del A è sbagliato";
+                return;
+            }
+            if (!double.TryParse(CefB.Text, out b))
+            {
+                Result.Text = "Input del B è sbagliato";
+                return;
+            }
+            if (!double.TryParse(CefC.Text, out c))
+            {
+                Result.Text = "Input del C è sbagliato";
+                return;
+            }
+
+            double res = Math.Pow(b, 2) - 4 * a * c;
+            Result.Text = res.ToString();
+        }
+    }
+}
